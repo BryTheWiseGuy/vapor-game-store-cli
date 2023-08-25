4dd3f05d3f502a9d572c05d3ddd54444c88910fd
Brian Ashman <brian.ashman@comcast.net>
2023-08-25 16:47:24 -0700

    Adds ipdb debug script to debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,2 +1,12 @@
+from db.models import User, Game, User_library
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import ipdb
 
-                
+if __name__=='__main__':
+    engine = create_engine('sqlite:///db/vapor_store.db')
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    ipdb.set_trace()
